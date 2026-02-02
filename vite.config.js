2c6fc0103008b935f2f37ff9cd5a3ff17555dee4
shahinr-r <rtlthemewordpress@gmail.com>
2026-02-02 18:09:14 +0330

    fix: vite base path for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',            // 👈 این خط را اضافه کردیم
   plugins: [react()],
   server: {
     host: '127.0.0.1',
